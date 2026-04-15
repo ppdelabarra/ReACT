378abcfb6975ec5e12a6bc3c08d70937ddf67229
ppdelabarra <30326264+ppdelabarra@users.noreply.github.com>
2026-04-15 21:33:39 +0200

    Delete src/sensor-reading/calibration.h

--- src/sensor-reading/calibration.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALIBRATION_H
-#define CALIBRATION_H
-
-float calibrateWind(int adc);
-float calibrateNoise(float vrms);
-float calibrateLux(float lux);
-float calibrateGlobeTemp(float t);
-
-#endif
