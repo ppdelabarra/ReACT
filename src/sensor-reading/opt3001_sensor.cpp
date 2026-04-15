f81dbbe7267c057db11a17bbd4050254cdce2ddc
ppdelabarra <30326264+ppdelabarra@users.noreply.github.com>
2026-04-15 16:11:54 +0200

    Configure OPT3001 sensor on initialization
    Initialize sensor configuration with specific settings.

--- src/sensor-reading/opt3001_sensor.cpp
+++ src/sensor-reading/opt3001_sensor.cpp
@@ -2,6 +2,14 @@
 
 void OPT3001Sensor::begin() {
     sensor.begin(0x45);
+
+    OPT3001_Config newConfig;
+    newConfig.RangeNumber = B1100;
+    newConfig.ConvertionTime = B0;
+    newConfig.Latch = B1;
+    newConfig.ModeOfConversionOperation = B11; // Continuous mode
+
+    sensor.writeConfig(newConfig);
 }
 
 float OPT3001Sensor::readLux() {
